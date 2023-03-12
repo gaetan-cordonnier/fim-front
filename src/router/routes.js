48a89d37cfc74b2583d4bd38bac87a9468a6cd02
gaetan-cordonnier <gaetan.cordonnier@gmail.com>
2023-03-12 12:21:46 +0100

    Add all pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,36 @@ const routes = [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
   },
+  {
+    path: "/add-food",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/AddFoodPage.vue") }],
+  },
+  {
+    path: "/remove-food",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/RemoveFoodPage.vue") }],
+  },
+  {
+    path: "/add-recipe",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/AddRecipePage.vue") }],
+  },
+  {
+    path: "/cooking",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/CookingPage.vue") }],
+  },
+  {
+    path: "/menu",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/MenuPage.vue") }],
+  },
+  {
+    path: "/shopping-list",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/ShoppingListPage.vue") }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
